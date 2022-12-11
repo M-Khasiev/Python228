9e7605d1a03eeda649a0e191b1ed740ddf891eba
Malik <2.mkhs.5@mail.ru>
2022-12-11 03:38:34 +0300

    New Classwork

--- main.py
+++ main.py
@@ -1102,6 +1102,7 @@ from random import *
 
 import time as t
 
+
 # second = t.time()
 # print("Секунды с начала цифровой эпохи:", second)
 # localtime = t.ctime()
@@ -3873,3 +3874,166 @@ import time as t
 # h1.set_birthday("23.01.1987")
 # print(h1.get_birthday())
 # h1.print_info()
+
+
+# Classwork 27
+
+# class Person:
+#     skill = 10
+#
+#     def __init__(self, name, surname):
+#         self.name = name
+#         self.surname = surname
+#
+#     def print_info(self):
+#         print("Данные сотрудника: ", self.name, self.surname)
+#
+#     def add_skill(self, k):
+#         self.skill += k
+#         print("Квалификация сотрудника: ", self.skill, "\n")
+#
+#
+# p1 = Person("Viktor", "Reznik")
+# p1.print_info()
+# p1.add_skill(3)
+#
+# p2 = Person("Anna", "Dol")
+# p2.print_info()
+# p2.add_skill(2)
+
+
+# class Point:
+#     # def __new__(cls, *args, **kwargs):
+#     #     print("Конструктор")
+#     #     return super().__new__(cls)
+#
+#     def __init__(self, x, y):
+#         print("Инициализатор")
+#         self.x = x
+#         self.y = y
+#
+#     def __del__(self):
+#         print("Удаление экземпляра", self.__class__.__name__)
+#
+#     def print_coord(self):
+#         print(f"x: {self.x}, y: {self.y}")
+#
+#
+# p1 = Point(5, 6)
+# p1.print_coord()
+# print(id(p1))
+#
+# p2 = Point(2, 7)
+# p2.print_coord()
+# print(id(p2))
+
+# class Point:
+#     count = 0
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#         Point.count += 1
+#
+#
+# p1 = Point(5, 10)
+# print(p1.count)
+# p2 = Point(7, 2)
+# print(p2.count)
+# p3 = Point(3, 4)
+# print(p3.count)
+# print(Point.count)
+# print(p1.count)
+
+
+# class Robot:
+#     k = 0
+#
+#     def __init__(self, name):
+#         self.name = name
+#         print("Инициализация робота:", self.name)
+#         Robot.k += 1
+#
+#     def __del__(self):
+#         print(self.name, "выключается!")
+#         Robot.k -= 1
+#
+#         if Robot.k == 0:
+#             print(self.name, "был последним")
+#         else:
+#             print("Работающий роботов осталось:", Robot.k)
+#
+#     def say_hi(self):
+#         print("Приветствую! Меня зовут", self.name)
+#
+#
+# droid1 = Robot("R2-D2")
+# droid1.say_hi()
+# print("Численность роботов:", Robot.k)
+# droid2 = Robot("C-3PO")
+# droid2.say_hi()
+# print("Численность роботов:", Robot.k)
+#
+# print("\nЗдесь роботы могут проделать какую-то работу\n")
+# print("Роботы закончили свою работу. Давайте их выключим.")
+# del droid1
+# del droid2
+# print("Численность роботов:", Robot.k)
+
+
+# class Point:
+#     def __init__(self, x, y):
+#         self.__x = self.__y = 0
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x  # _Point__x
+#             self.__y = y
+#
+#     def __check_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def set_coord(self, x, y):
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами!")
+#
+#     def set_coord_x(self, x):
+#         if Point.__check_value(x):
+#             self.__x = x
+#         else:
+#             print("Координаты должны быть числами!")
+#
+#     def set_coord_y(self, y):
+#         if Point.__check_value(y):
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами!")
+#
+#     def get_coord(self):
+#         return self.__x, self.__y
+#
+#     def get_coord_x(self):
+#         return self.__x
+#
+#     def get_coord_y(self):
+#         return self.__y
+#
+#
+# p1 = Point(5, 10)
+# p1._Point__x = 111
+# print(p1.get_coord())
+# p1.set_coord(1, 2.3)
+# print(p1.get_coord())
+# print(p1.__dict__)
+# p1.set_coord_x(8)
+# print(p1.get_coord_x())
+# p1.set_coord_y(9)
+# print(p1.get_coord_y())
+# p1.__x = 100
+# p1.__y = 'abc'
+# print(p1.x, p1.y)
+# print(p1.__dict__)
+# print(p1.__dict__)
