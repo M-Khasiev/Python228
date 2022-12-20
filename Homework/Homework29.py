391080f7890d34e6a40490b56c744de1f062d8a2
Malik <2.mkhs.5@mail.ru>
2022-12-21 01:21:34 +0300

    NEW HOMEWORK

---
+++ Homework/Homework29.py
@@ -0,0 +1,254 @@
+#  Вариант № 1
+class Account:
+    rate_usd = 0.013
+    rate_eur = 0.011
+    suffix = "RUB"
+    suffix_usd = "USD"
+    suffix_eur = "EUR"
+
+    def __init__(self, num, surname, percent, value=0):
+        self.__num = num
+        self.__surname = surname
+        self.__percent = percent
+        self.__value = value
+        print(f"Счёт #{self.__num} принадлежащий {self.__surname} был открыт.")
+        print("*" * 50)
+
+    def __del__(self):
+        print("*" * 50)
+        print(f"Счёт #{self.__num} принадлежащий {self.__surname} был закрыт.")
+
+    @classmethod
+    def set_usd_rate(cls, rate):
+        cls.rate_usd = rate
+
+    @classmethod
+    def set_eur_rate(cls, rate):
+        cls.rate_eur = rate
+
+    @staticmethod
+    def convert(value, rate):
+        return value * rate
+
+    def get_num(self):
+        return self.__num
+
+    def set_num(self, a):
+        self.__num = a
+
+    def get_surname(self):
+        return self.__surname
+
+    def set_surname(self, a):
+        self.__surname = a
+
+    def get_percent(self):
+        return self.__percent
+
+    def set_percent(self, a):
+        self.__percent = a
+
+    def get_value(self):
+        return self.__value
+
+    def set_value(self, a):
+        self.__value = a
+
+    def add_percents(self):
+        self.__value += self.__value * self.__percent
+        print("Проценты были успешно начислены!")
+        self.print_balance()
+
+    def withdraw_money(self, val):
+        if val > self.__value:
+            print(f"К сожалению у вас нет {val} {Account.suffix}")
+        else:
+            self.__value -= val
+            print(f"{val} {Account.suffix} было успешно снято!")
+        self.print_balance()
+
+    def add_money(self, val):
+        self.__value += val
+        print(f"{val} {Account.suffix} было успешно добавлено!")
+        self.print_balance()
+
+    def convert_to_usd(self):
+        usd_val = Account.convert(self.__value, Account.rate_usd)
+        print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
+
+    def convert_to_eur(self):
+        eur_val = Account.convert(self.__value, Account.rate_eur)
+        print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
+
+    def print_balance(self):
+        print(f"Текущий баланс {self.__value} {Account.suffix}")
+
+    def print_info(self):
+        print("Информация о счёте")
+        print("-" * 20)
+        print(f"#{self.__num}")
+        print(f"Владелец: {self.__surname}")
+        self.print_balance()
+        print(f"Проценты: {self.__percent:.0%}")
+        print("-" * 20)
+
+
+acc = Account('12345', 'Долгих', 0.03, 1000)
+print(acc.get_num())
+print(acc.get_surname())
+print(acc.get_percent())
+print(acc.get_value())
+print()
+acc.print_info()
+acc.convert_to_usd()
+acc.convert_to_eur()
+print()
+Account.set_usd_rate(2)
+acc.convert_to_usd()
+Account.set_eur_rate(3)
+acc.convert_to_eur()
+print()
+acc.set_surname("Дюма")
+acc.print_info()
+print()
+acc.add_percents()
+print()
+acc.withdraw_money(3000)
+print()
+acc.withdraw_money(100)
+print()
+acc.add_money(5000)
+print()
+acc.withdraw_money(3000)
+
+
+#  Вариант № 2
+class Account:
+    rate_usd = 0.013
+    rate_eur = 0.011
+    suffix = "RUB"
+    suffix_usd = "USD"
+    suffix_eur = "EUR"
+
+    def __init__(self, num, surname, percent, value=0):
+        self.__num = num
+        self.__surname = surname
+        self.__percent = percent
+        self.__value = value
+        print(f"Счёт #{self.__num} принадлежащий {self.__surname} был открыт.")
+        print("*" * 50)
+
+    def __del__(self):
+        print("*" * 50)
+        print(f"Счёт #{self.__num} принадлежащий {self.__surname} был закрыт.")
+
+    @classmethod
+    def set_usd_rate(cls, rate):
+        cls.rate_usd = rate
+
+    @classmethod
+    def set_eur_rate(cls, rate):
+        cls.rate_eur = rate
+
+    @staticmethod
+    def convert(value, rate):
+        return value * rate
+
+    @property
+    def num(self):
+        return self.__num
+
+    @num.setter
+    def num(self, a):
+        self.__num = a
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @surname.setter
+    def surname(self, a):
+        self.__surname = a
+
+    @property
+    def percent(self):
+        return self.__percent
+
+    @percent.setter
+    def percent(self, a):
+        self.__percent = a
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, a):
+        self.__value = a
+
+    def add_percents(self):
+        self.__value += self.__value * self.__percent
+        print("Проценты были успешно начислены!")
+        self.print_balance()
+
+    def withdraw_money(self, val):
+        if val > self.__value:
+            print(f"К сожалению у вас нет {val} {Account.suffix}")
+        else:
+            self.__value -= val
+            print(f"{val} {Account.suffix} было успешно снято!")
+        self.print_balance()
+
+    def add_money(self, val):
+        self.__value += val
+        print(f"{val} {Account.suffix} было успешно добавлено!")
+        self.print_balance()
+
+    def convert_to_usd(self):
+        usd_val = Account.convert(self.__value, Account.rate_usd)
+        print(f"Состояние счёта: {usd_val} {Account.suffix_usd}")
+
+    def convert_to_eur(self):
+        eur_val = Account.convert(self.__value, Account.rate_eur)
+        print(f"Состояние счёта: {eur_val} {Account.suffix_eur}")
+
+    def print_balance(self):
+        print(f"Текущий баланс {self.__value} {Account.suffix}")
+
+    def print_info(self):
+        print("Информация о счёте")
+        print("-" * 20)
+        print(f"#{self.__num}")
+        print(f"Владелец: {self.__surname}")
+        self.print_balance()
+        print(f"Проценты: {self.__percent:.0%}")
+        print("-" * 20)
+
+
+acc = Account('12345', 'Долгих', 0.03, 1000)
+print(acc.num)
+print(acc.surname)
+print(acc.percent)
+print(acc.value)
+print()
+acc.print_info()
+acc.convert_to_usd()
+acc.convert_to_eur()
+print()
+Account.set_usd_rate(2)
+acc.convert_to_usd()
+Account.set_eur_rate(3)
+acc.convert_to_eur()
+print()
+acc.surname = "Дюма"
+acc.print_info()
+print()
+acc.add_percents()
+print()
+acc.withdraw_money(3000)
+print()
+acc.withdraw_money(100)
+print()
+acc.add_money(5000)
+print()
+acc.withdraw_money(3000)
---
+++ Homework/Homework30.py
@@ -0,0 +1,127 @@
+import re
+
+
+class Automobile:
+    def __init__(self, model, year, manufacturer, engine, color, price):
+        self.model = model
+        self.year = year
+        self.manufacturer = manufacturer
+        self.engine = engine
+        self.color = color
+        self.price = price
+
+    @property
+    def model(self):
+        return self.__model
+
+    @model.setter
+    def model(self, m):
+        self.verify_model(m)
+        self.__model = m
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, y):
+        self.verify_year(y)
+        self.__year = y
+
+    @property
+    def manufacturer(self):
+        return self.__manufacturer
+
+    @manufacturer.setter
+    def manufacturer(self, m):
+        self.verify_manufacturer(m)
+        self.__manufacturer = m
+
+    @property
+    def engine(self):
+        return self.__engine
+
+    @engine.setter
+    def engine(self, e):
+        self.verify_engine(e)
+        self.__engine = e
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, c):
+        self.verify_color(c)
+        self.__color = c
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, p):
+        self.verify_price(p)
+        self.__price = p
+
+    @staticmethod
+    def verify_model(model):
+        if not isinstance(model, str):
+            raise TypeError("Модель автомобиля должна быть строкой")
+
+    @staticmethod
+    def verify_year(year):
+        if not isinstance(year, str) or len(year) != 4:
+            raise TypeError("Год выпуска должен быть строкой и четырехзначным числом")
+        for i in year:
+            if not i.isdigit():
+                raise TypeError("Использовать в строке только числа!")
+        r = re.findall(r'^(19[\d][\d]|20[0-2][\d])$', year)
+        if not r:
+            raise TypeError("Некорректно указан год")
+
+    @staticmethod
+    def verify_manufacturer(manufacturer):
+        if not isinstance(manufacturer, str):
+            raise TypeError("Название производителя может быть только строкой")
+
+    @staticmethod
+    def verify_engine(engine):
+        if not isinstance(engine, str):
+            raise TypeError("Мощность двигателя должна быть строкой")
+
+    @staticmethod
+    def verify_color(color):
+        if not isinstance(color, str):
+            raise TypeError("Цвет машины должен быть строкой")
+        for i in color:
+            if not i.isalpha():
+                raise TypeError("Название цвета должен быть из букв!")
+
+    @staticmethod
+    def verify_price(price):
+        if not isinstance(price, int):
+            raise TypeError("Цена должна быть числом")
+
+    def print_info(self):
+        print(str.center(' Данные автомобиля ', 40, "*"))
+        print(f"Название модели: {self.__model}")
+        print(f"Год выпуска: {self.year}")
+        print(f"Производитель: {self.manufacturer}")
+        print(f"Мощность двигателя: {self.engine}")
+        print(f"Цвет машины: {self.color}")
+        print(f"Цена: {self.price}")
+        print("=" * 40)
+
+
+auto = Automobile("X7 M50i", "2021", "BMW", "530 л.c.", "White", 10790000)
+auto.print_info()
+auto.model = "S63 E Perfomance"
+auto.year = "2023"
+auto.manufacturer = "Mercedes-Benz"
+auto.engine = "802 л.с."
+auto.color = "Black"
+auto.price = 31120000
+auto.print_info()
+
+
