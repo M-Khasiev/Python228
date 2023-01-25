dadab2e63e6f397dc8821f86cd5acfb9b6ddafff
Malik <2.mkhs.5@mail.ru>
2023-01-26 02:30:37 +0300

    New HM37

---
+++ Homework/Homework37.py
@@ -0,0 +1,68 @@
+import json
+
+
+# Вариант 1 (Статические свойства)
+
+def annual_income(summa: int):
+    return summa
+
+
+class PersonData:
+    name = 'John'
+    surname = 'Wick'
+    age = 42
+    resides = 'New York'
+    cars = ['Mercedes-Benz', 'Audi', 'Mustang']
+    children = ('Mark', 'Bella', 'Joseph')
+    foot_size = 43.5
+    married = True
+    education = {
+        'Stanford University': "Bachelor's degree",
+        'Harvard University': "Master's degree",
+        'Time spent on education': {
+            'Stanford': 3,
+            'Harvard': 1
+        }
+    }
+    income = annual_income(100000)
+
+
+dt = dict(PersonData.__dict__)
+lst = list()
+for i, j in dt.items():
+    if i.startswith("__"):
+        lst.append(i)
+for i in lst:
+    del dt[i]
+json_string = json.dumps(dt)
+data = json.loads(json_string)
+print(data)
+print()
+
+
+# Вариант 2 (Динамические свойства)
+
+class PersonData2:
+    def __init__(self, name, surname, age, resides, cars, children, foot_size, married, education):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.resides = resides
+        self.cars = cars
+        self.children = children
+        self.foot_size = foot_size
+        self.married = married
+        self.education = education
+        self.income = self.annual_income2(100000)
+
+    @staticmethod
+    def annual_income2(summa: int):
+        return summa
+
+
+p1 = PersonData2('John', 'Wick', 42, 'New York', ['Mercedes-Benz', 'Audi', 'Mustang'], ('Mark', 'Bella', 'Joseph'),
+                 43.5, True, {'Stanford University': "Bachelor's degree", 'Harvard University': "Master's degree",
+                              'Time spent on education': {'Stanford': 3, 'Harvard': 1}})
+json_string2 = json.dumps(p1.__dict__)
+data2 = json.loads(json_string2)
+print(data2)
